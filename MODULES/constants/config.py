7391ba6eac42f0a45716665be957369bec5c1624
v <t.shagin@gmail.com>
2024-05-21 17:10:36 +0300

    feat: Токены внесены в не обновляющийся на github файл config.py

---
+++ MODULES/constants/config.py
@@ -0,0 +1,3 @@
+from typing import Final
+
+MAIN_BOT_TOKEN: Final[str] = '6848386363:AAGAVW-SueX-JxddIsHjXmYDxBvNvyvbR9c'
--- MODULES/constants/reg_variables/BOT.py
+++ MODULES/constants/reg_variables/BOT.py
@@ -1,5 +1,7 @@
+from MODULES.constants.config import MAIN_BOT_TOKEN
+
 from typing import Final
 from telebot import TeleBot
 
 
-GUARD: Final[TeleBot] = TeleBot("6848386363:AAGAVW-SueX-JxddIsHjXmYDxBvNvyvbR9c")
+GUARD: Final[TeleBot] = TeleBot(MAIN_BOT_TOKEN)
