e2515e342eba2bc4f5981e36561882ced9db3ad5
v <t.shagin@gmail.com>
2024-06-04 18:33:35 +0300

    feat: Доделан модуль рассылок
    Изменения:
    * Доделан модуль рассылок
    * Произведена миграция базы данных

--- MODULES/database/models/mailing.py
+++ MODULES/database/models/mailing.py
@@ -3,6 +3,7 @@ from peewee import CharField, TimeField
 
 
 class Mailing(BaseModelWithoutLogging):
+    type = CharField()
     message_json = CharField()
     buttons_json = CharField()
     send_time = TimeField()
--- MODULES/domain/ads_executors/mailing_sender.py
+++ MODULES/domain/ads_executors/mailing_sender.py
@@ -1,20 +1,39 @@
+import time
 import schedule
 
+from telebot.types import Message, InlineKeyboardMarkup
 from MODULES.database.models.mailing import Mailing
 from MODULES.database.models.users import Authors
+import MODULES.domain.user_request_executors.util as ure_util
 
 
 class MailingSender:
-    def __init__(self, message):
-        self.message = message
+    def __init__(self, message: Message):
+        self.message: Message = message
+        self.work: bool = True
 
     def schedule_send(self):
         for msg in Mailing.select():
             schedule.every().day.at(str(msg.send_time)).do(self.send, msg.id).tag(str(msg.id))
 
-    def send(self, id_):
+    @staticmethod
+    def send(id_):
+        mailing: Mailing = Mailing.get_by_id(id_)
         for uid in map(lambda x: x.chat_id, Authors.select()):
-            pass
+            ure_util.send(
+                type=mailing.type,
+                kwargs_json=mailing.message_json,
+                markup=InlineKeyboardMarkup.de_json(mailing.buttons_json),
+                chat_id=uid
+            )
+        schedule.clear(str(id_))
+
+    def restart(self):
+        self.work = False
+        time.sleep(1)
+        self()
 
     def __call__(self):
-        pass
+        self.schedule_send()
+        while self.work:
+            schedule.run_pending()
